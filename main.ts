01d7227b9e25b98a5a4bc075ecda1460e9aa7bfb
gearhabr26 <127230354+gearhabr26@users.noreply.github.com>
2023-04-18 10:34:42 -0400

    Update main.ts, main.blocks

--- main.ts
+++ main.ts
@@ -2,14 +2,12 @@ let how_long_is_the_circuit_broken_at_P0 = 0
 basic.forever(function () {
     if (!(input.pinIsPressed(TouchPin.P0))) {
         basic.pause(20)
-        how_long_is_the_circuit_broken_at_P0 += 1
+        how_long_is_the_circuit_broken_at_P0 += 20
         if (how_long_is_the_circuit_broken_at_P0 > 1000) {
             radio.sendNumber(1)
             WaitUntilBlocks.waitUntilButtonPressed(Button.A)
-        } else {
-        	
         }
     } else {
-    	
+        how_long_is_the_circuit_broken_at_P0 = 0
     }
 })
