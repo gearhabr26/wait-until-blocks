8fbffb5bd51aa4d24701cf0b28bfce6d9542ab34
gearhabr26 <127230354+gearhabr26@users.noreply.github.com>
2023-04-18 10:29:03 -0400

    Update pxt.json, main.ts, main.blocks

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-
+let how_long_is_the_circuit_broken_at_P0 = 0
+basic.forever(function () {
+    if (!(input.pinIsPressed(TouchPin.P0))) {
+        basic.pause(20)
+        how_long_is_the_circuit_broken_at_P0 += 1
+        if (how_long_is_the_circuit_broken_at_P0 > 1000) {
+            radio.sendNumber(1)
+            WaitUntilBlocks.waitUntilButtonPressed(Button.A)
+        } else {
+        	
+        }
+    } else {
+    	
+    }
+})
